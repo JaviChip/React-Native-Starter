ddb8d1065fffb8b60f77e74c53aab5cae145c8bd
javi <javi@Javis-MacBook-Pro-3.local>
2024-04-16 17:56:25 -0700

    Add navigation, hero, async, and loading

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation/navigation';
 
 export default function App() {
   return (
