c6f2db9fa8754e5a2ff1f395de3867ba3dd4d6dd
javi <javi@Javis-MacBook-Pro-3.local>
2024-05-08 13:22:51 -0700

    Add basic functionality

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,16 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "../screens/HomeScreen"
+import { View, Text, LogBox } from 'react-native'
+import React from 'react'
 
 const Stack = createNativeStackNavigator();
 
-import { View, Text } from 'react-native'
-import React from 'react'
+LogBox.ignoreLogs([
+
+  'Non-serializable values were found in the navigation state',
+
+]);
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" options = {{headerShown: false}} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className = "flex-1 relative">
+
+      <StatusBar style="light"/>
+
     </View>
   )
 }
